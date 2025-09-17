0c3b6d1998a670dbc8419b1af2c077cd598e1d88
Amiri <publicamiri@gmail.com>
2025-09-17 12:26:16 -0400

    chore: remove Zone.Identifier junk files

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from '../pages/HomePage.jsx'
 import SpacecraftsPage from '../pages/SpacecraftsPage.jsx'
 import SpacecraftPage from '../pages/SpacecraftPage.jsx'
@@ -8,14 +8,16 @@ import PlanetsPage from '../pages/PlanetsPage.jsx'
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/spacecrafts" element={<SpacecraftsPage />} />
-      <Route path="/spacecrafts/build" element={<ConstructPage />} />
-      <Route path="/spacecrafts/:id" element={<SpacecraftPage />} />
-      <Route path="/planets" element={<PlanetsPage />} />
-      {/* Redirect any unknown route to home */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/spacecrafts" element={<SpacecraftsPage />} />
+        <Route path="/spacecrafts/build" element={<ConstructPage />} />
+        <Route path="/spacecrafts/:id" element={<SpacecraftPage />} />
+        <Route path="/planets" element={<PlanetsPage />} />
+        {/* Redirect any unknown route to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
   )
 }
